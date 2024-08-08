1b6929f3ae846890482667d2050f2a2ceca182f2
Dennis Karungu <dennismburu10@gmail.com>
2024-08-08 20:32:24 +0300

    Update testimony.tsx

--- app/components/testimony.tsx
+++ app/components/testimony.tsx
@@ -39,7 +39,7 @@ export const Testimonials = () => {
       content: (
         <div>
           <p>
-            My cursory audit of <TwitterHandle>@chronark_</TwitterHandle>'s envshare:
+            My cursory audit of <TwitterHandle>@dennismburu201</TwitterHandle>'s depo:
           </p>
           <p>
             It is light, extremely functional, and does its symmetric block cipher correctly, unique initialization
@@ -84,7 +84,7 @@ export const Testimonials = () => {
       content: (
         <div>
           <p>
-            Congratulations on the launch <TwitterHandle>@chronark_</TwitterHandle>👏! This is such a valuable product
+            Congratulations on the launch <TwitterHandle>@dennismburu20</TwitterHandle>👏! This is such a valuable product
             for developers. Icing on the cake is that it's open source! ✨
           </p>
         </div>
